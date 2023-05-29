4305d809b5b9c8a11bb1106b63bf09309ffa5dc3
Đại Trần Đình <trandinhdai_t64@hus.edu.vn>
2023-05-29 22:34:57 +0700

    spring project

---
+++ ceremony/src/main/java/com/hus/ceremony/CeremonyApplication.java
@@ -0,0 +1,13 @@
+package com.hus.ceremony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeremonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeremonyApplication.class, args);
+	}
+
+}
---
+++ ceremony/src/test/java/com/hus/ceremony/CeremonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hus.ceremony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeremonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
